b7829490f3ee6dd509064e35c62623ec94e2fccc
kieutrinh334 <duongngockieutrinh0303@gmail.com>
2026-01-11 11:04:51 +0700

    fix transaction_date format

--- spark_streaming.py
+++ spark_streaming.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json, expr, lit, concat
+from pyspark.sql.functions import col, from_json, expr, lit, to_date, when, length, concat
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
 from exchange_rate import get_vnd_rate
 
@@ -65,14 +65,16 @@ fraud_df = parsed_df.filter(col("is_fraud") == "Yes")
 # CHUẨN HÓA DATETIME
 valid_df = valid_df.withColumn(
     "transaction_date",
-    expr("""
-        try_to_timestamp(
-            concat_ws('/', day, month, year),
-            'dd/MM/yyyy'
-        )
-    """)
+    to_date(
+        expr("concat_ws('/', day, month, year)"),
+        "d/M/yyyy"
+    )
 )
-valid_df = valid_df.withColumn("time", expr("concat(time, ':00')"))
+valid_df = valid_df.withColumn(
+    "time",
+    when(length(col("time")) == 5, concat(col("time"), lit(":00")))
+    .otherwise(col("time"))
+)
 
 
 # TÍNH AMOUNT_VND
